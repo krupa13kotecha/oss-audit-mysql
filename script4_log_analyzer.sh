fdce03f389df052f589e2dcce02200af0b7b29ee
krupa13kotecha <krupa13kotecha@gmail.com>
2026-03-31 13:49:12 +0530

    Update script4_log_analyzer.sh

--- script4_log_analyzer.sh
+++ script4_log_analyzer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 4: Log File Analyzer
-# Author: Krishna Kumar Sahu | Reg: 24MIM10150
+#  Author: Krupa Kotecha | Reg: 24BCE11262
 # Course: Open Source Software (OSS NGMC)
 # Purpose: Read a log file line-by-line, count keyword occurrences,
 #          and display a summary with the last 5 matching lines.
